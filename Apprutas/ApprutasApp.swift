810a38262dd87721f76dce14d6b8d38a4f27d7d2
Dasha Liubivaya <dasalubivaa@gmail.com>
2024-12-25 19:39:49 +0300

    Initial Commit

---
+++ Apprutas/ApprutasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApprutasApp.swift
+//  Apprutas
+//
+//  Created by Дарья Любивая on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ApprutasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
